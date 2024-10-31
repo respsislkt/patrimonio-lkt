7a079dc284e909a6e8eb60be69770139812a7707
respsistlukat <respsislukat@gmail.com>
2024-10-31 15:12:17 -0500

    }

--- funciones.js
+++ funciones.js
@@ -5,3 +5,9 @@ function doGet(){
 function obtenerDatosHTML(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerRazon(){
+    let hoja = SpreadsheetApp.openById('1yj1NK8uygrh34F0kiggkI8zAwTpJifIZv8hsiU9b-xo').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
